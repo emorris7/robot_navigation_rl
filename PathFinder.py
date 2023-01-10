3111f410d7e0ec05e0c3cacd4513b9db607200c3
Emily Morris <emilymorris@Emilys-MacBook-Air-9.local>
2023-01-10 02:07:12 +0200

    Experiment just trying to get to point no angle, but with angle diff as observation, sus stuff happens

--- PathFinder.py
+++ PathFinder.py
@@ -1,5 +1,6 @@
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray.rllib.algorithms.sac import SACConfig
+from ray.rllib.algorithms.mbmpo import MBMPOConfig
 from ray.tune.logger import pretty_print
 from env.SimpleEnvironment import SimpleRobotEnviroment
 from env.SimpleEnvironment_condensed_obs import SimpleRobotEnviromentCO
@@ -104,9 +105,9 @@ if __name__ == '__main__':
     # )
     # print(algo.config.horizon)
     # print("m")
-
+    
     # algo = (
-    #     SACConfig()
+    #     MBMPOConfig()
     #     .rollouts(num_rollout_workers=8,horizon=600)
     #     .resources(num_gpus=0)
     #     .environment(SimpleRobotEnviromentCO, env_config={"render_mode":"rgb_array"})
@@ -114,16 +115,25 @@ if __name__ == '__main__':
     #     .framework(framework="torch")
     #     .build()
     # )
-
     algo = (
         SACConfig()
         .rollouts(num_rollout_workers=8,horizon=600)
         .resources(num_gpus=0)
-        .environment(SimpleRobotEnviroment, env_config={"render_mode":"rgb_array"})
-        .callbacks(GoalCallbacks)
+        .environment(SimpleRobotEnviromentCO, env_config={"render_mode":"rgb_array"})
+        .callbacks(GoalCallbacksCO)
         .framework(framework="torch")
         .build()
     )
+
+    # algo = (
+    #     SACConfig()
+    #     .rollouts(num_rollout_workers=8,horizon=600)
+    #     .resources(num_gpus=0)
+    #     .environment(SimpleRobotEnviroment, env_config={"render_mode":"rgb_array"})
+    #     .callbacks(GoalCallbacks)
+    #     .framework(framework="torch")
+    #     .build()
+    # )
 
     # algo = (
     #     SACConfig()
@@ -135,8 +145,8 @@ if __name__ == '__main__':
     #     .build()
     # )
     
-    #For testing
-    # algo.restore("/Users/emilymorris/ray_results/SAC_SimpleRobotEnviromentCO_2023-01-09_22-50-52o3se9bhz/checkpoint_003281/")
+    # For testing
+    # algo.restore("/Users/emilymorris/ray_results/SAC_SimpleRobotEnviromentCO_2023-01-09_22-50-52o3se9bhz/checkpoint_006691/")
 
     # num_episodes = 6000
     # for i in range(num_episodes):
@@ -183,12 +193,12 @@ if __name__ == '__main__':
     #     print(i)
     #     if not done:
     #         action = algo.compute_single_action(obs)
-    #         # print("Action: ", action)
+    #         print("Action: ", action)
     #         obs, reward, done, _ = env.step(action)
-    #         # print("ROBOT: ", env.robot.pose)
-    #         # print("Observation:",obs)
-    #         # print("Reward: ", reward)
-    #         # print("Done",done)
+    #         print("ROBOT: ", env.robot.pose)
+    #         print("Observation:",obs)
+    #         print("Reward: ", reward)
+    #         print("Done",done)
     #     else:
     #         print("Done")
 
